5ac141264a9719e8cff605ecb320485fd5281cb2
Robobyte Studios <nk.kavinkrishna@gmail.com>
2025-08-15 23:22:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2411,53 +2411,7 @@ def passfr():
     
     import streamlit as st
 
-    placeholder = st.empty()
-    placeholder1 = st.empty()
-    
-    
-    if "log" not in st.session_state:
-        
-        if placeholder.button("Login with google"):
-    
-            st.login("google") 
-            
-        if st.user.is_logged_in:
-        # Code here runs just after login completes successfully
-            st.write(f"Hello, {st.user.name}!")
-            
-            st.session_state.log = 1
-            placeholder.empty()
-            if placeholder1.button("Logout" , width=150):
-    
-                placeholder1.empty()
-    
-                del st.session_state['log']
-    
-                st.logout()
-    
-                
-            passfr()
-        # Your post-login code here
-        else:
-            
-            st.write("Please log in.")
-        
-    
-        
-    else:
-    
-        st.write(f"Hello, {st.user.name}!")
-    
-        if placeholder1.button("Logout" , width=150):
-    
-                placeholder1.empty()
-    
-                del st.session_state['log']
-    
-                st.logout()
-        
-    
-        passfr()
+   
     
     st.sidebar.markdown("---")
     st.sidebar.caption("Connect with me")
@@ -4038,8 +3992,53 @@ def passfr():
     """)
 
 
-
+ placeholder = st.empty()
+    placeholder1 = st.empty()
     
+    
+    if "log" not in st.session_state:
+        
+        if placeholder.button("Login with google"):
+    
+            st.login("google") 
+            
+        if st.user.is_logged_in:
+        # Code here runs just after login completes successfully
+            st.write(f"Hello, {st.user.name}!")
+            
+            st.session_state.log = 1
+            placeholder.empty()
+            if placeholder1.button("Logout" , width=150):
+    
+                placeholder1.empty()
+    
+                del st.session_state['log']
+    
+                st.logout()
+    
+                
+            passfr()
+        # Your post-login code here
+        else:
+            
+            st.write("Please log in.")
+        
+    
+        
+    else:
+    
+        st.write(f"Hello, {st.user.name}!")
+    
+        if placeholder1.button("Logout" , width=150):
+    
+                placeholder1.empty()
+    
+                del st.session_state['log']
+    
+                st.logout()
+        
+    
+        passfr()
 
 
     
