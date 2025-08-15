9bf6fc75fe7b6b0d58d6acd7367f2c710c15fe01
Robobyte Studios <nk.kavinkrishna@gmail.com>
2025-08-15 12:38:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import streamlit as st
 
 from google_auth_oauthlib.flow import InstalledAppFlow # type: ignore
 
+api_key = st.secrets["o"]
+
 SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
-flow = InstalledAppFlow.from_client_secrets_file('secrets.toml', SCOPES)
+flow = InstalledAppFlow.from_client_secrets_file(api_key, SCOPES)
 credentials = flow.run_local_server(port=0)
 
 st.write("Test 123")
