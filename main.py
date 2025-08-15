e1cfc2504b129271b0d4f5609886a01c9c7a3a4d
Robobyte Studios <nk.kavinkrishna@gmail.com>
2025-08-15 20:24:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,9 @@
 import streamlit as st
-import tempfile
-from google_auth_oauthlib.flow import InstalledAppFlow
 
-SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
-json_str = st.secrets["o"]
+if st.button("Authenticate"):
 
-with tempfile.NamedTemporaryFile(mode='w+', suffix='.json', delete=False) as f:
-    f.write(json_str)
-    f.flush()
-    temp_file_path = f.name
+    st.login("google")
 
-flow = InstalledAppFlow.from_client_secrets_file(temp_file_path, SCOPES)
-auth_url, _ = flow.authorization_url(prompt='consent')
+    st.write("😇🤮😵‍💫")
 
-st.write("Please open this URL in your browser to authorize:")
-st.write(auth_url)
-
-# Now run_local_server without opening the browser to wait for redirect
-credentials = flow.run_local_server(port=0, open_browser=False)
-
-st.write("Login successful!")
-st.write(f"Access token: {credentials.token}")
+st.write(st.user["name"])
