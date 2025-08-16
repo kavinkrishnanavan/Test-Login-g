d7b19f9cffa7bfaa7a0e89890519502dba502970
Robobyte Studios <nk.kavinkrishna@gmail.com>
2025-08-16 10:34:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4019,49 +4019,6 @@ with placeholder:
     else:
         st.write("Please log in.")
     
-if "log" not in st.session_state:
-        
-    if placeholder.button("Login with google"):
-    
-        st.login("google") 
-            
-    if st.user.is_logged_in:
-        # Code here runs just after login completes successfully
-        #st.write(f"Hello, {st.user.name}!")
-            
-        st.session_state.log = 1
-        placeholder.empty()
-        if placeholder1.button("Logout" , width=150):
-    
-            placeholder1.empty()
-    
-            del st.session_state['log']
-    
-            st.logout()
-    
-                
-        passfr()
-        # Your post-login code here
-    else:
-            
-        st.write("Please log in.")
-        
-    
-        
-else:
-    
-    #st.write(f"Hello, {st.user.name}!")
-    
-    if placeholder1.button("Logout" , width=150):
-    
-            placeholder1.empty()
-    
-            del st.session_state['log']
-    
-            st.logout()
-        
-    
-    passfr()
 
 
     
