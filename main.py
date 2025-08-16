9abfe038aea6a2d312f8bbba533713c490edfc87
Robobyte Studios <nk.kavinkrishna@gmail.com>
2025-08-16 15:09:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4034,7 +4034,7 @@ else:
 
     st.markdown("---")
 
-    if st_image_button("Hello", "https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"):
+    if st_image_button("my_button", "https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg", width="50px", outlined=True):
 
         st.session_state.log = 1
 
