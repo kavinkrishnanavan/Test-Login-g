71e4e44c20a597605285150521effaa57aa95051
Robobyte Studios <nk.kavinkrishna@gmail.com>
2025-08-16 15:37:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4033,7 +4033,7 @@ else:
 
     img = Image.open("icon.png")
 
-    if st.button("Hello {i}".format(i = img)):
+    if st.button("Hello {i}".format(i = st.image(img))):
 
         st.session_state.log = 1
 
