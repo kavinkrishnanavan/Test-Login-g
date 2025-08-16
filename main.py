989b519605bfe61ee9a1d6958b9f92dcfc79351d
Robobyte Studios <nk.kavinkrishna@gmail.com>
2025-08-16 12:06:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4007,45 +4007,45 @@ def passfr():
 
 if "log" not in st.session_state:
         
-        if placeholder.button("Login with google"):
+    if placeholder.button("Login with google"):
         
-            st.login("google") 
+        st.login("google") 
                 
-        if st.user.is_logged_in:
+    if st.user.is_logged_in:
             # Code here runs just after login completes successfully
             #st.write(f"Hello, {st.user.name}!")
                 
-            st.session_state.log = 1
-            placeholder.empty()
-            if placeholder1.button("Logout" , width=150):
+        st.session_state.log = 1
+        placeholder.empty()
+        if placeholder1.button("Logout" , width=150):
         
-                placeholder1.empty()
+            placeholder1.empty()
         
-                del st.session_state['log']
+            del st.session_state['log']
         
-                st.logout()
+            st.logout()
         
                     
-            extra.call()
+        extra.call()
             # Your post-login code here
-        else:
+    else:
                 
-            st.write("Please log in.")
+        st.write("Please log in.")
             
         
             
-    else:
+else:
         
         #st.write(f"Hello, {st.user.name}!")
         
-        if placeholder1.button("Logout" , width=150):
+    if placeholder1.button("Logout" , width=150):
         
-                placeholder1.empty()
+            placeholder1.empty()
         
-                del st.session_state['log']
+            del st.session_state['log']
         
-                st.logout()
-        extra.call()
+            st.logout()
+    extra.call()
 
     
     
